9346ad77d6a130181a54ffb964bf78426be2cd7d
Mahdi Sabbaghi <47177191+Helloworld10011@users.noreply.github.com>
2021-01-01 22:12:03 +0330

    Delete be.py

--- RL-Berkeley/hw1/cs285/be.py
+++
@@ -1,6 +0,0 @@
-#import tensorflow_probability as tfp
-#import tensorflow as tf
-#x= tfp.distributions.Categorical(probs=[0.7, 0.1, 0.2]).log_prob(0)
-#print(tf.random.categorical(logits=[0.7, 0.1, 0.2], num_samples= 1))
-
-print(1//5)
